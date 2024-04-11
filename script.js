fd0359874c93ceacf953955ca9791efa7a942692
MohitMitra1984 <mohit.a.mitra@gmail.com>
2024-04-11 17:48:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+//defining the values
+const contentSection = document.querySelector("#slide")
+const ActionBtn = document.querySelector(".challange-button")
+const ReturnBtn = document.querySelector(".challange-return-button")
+
+//Event lisener for ActionBtn
+ActionBtn.addEventListener('click', SlideLeft)
+
+//Event lisenert for ReturnBtn
+ReturnBtn.addEventListener('click', SlideRight)
+
+function SlideLeft(){
+     if (contentSection) { 
+        contentSection.classList.add('content-slide-left');
+    } else {
+        console.log("Error: contentSection not found.");
+    }
+}
+
+function SlideRight(){
+    if (contentSection) { 
+        contentSection.classList.remove('content-slide-left');
+    } else {
+        console.log("Error: contentSection not found.");
+    }
+}
